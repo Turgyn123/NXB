2f8e844725b07faee93af63db0dfd6748f3928e5
Pirata <19861321+Pirata156@users.noreply.github.com>
2025-03-23 17:40:18 +0000

    Pushing initial project documents

---
+++ common/src/main/java/net/narutoxboruto/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.narutoxboruto;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "narutoxboruto";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
