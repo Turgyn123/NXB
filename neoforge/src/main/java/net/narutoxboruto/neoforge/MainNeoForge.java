9c38d5473d85802360790ca4e110479842c12eb3
Turgyn <130315908+Turgyn123@users.noreply.github.com>
2025-03-23 18:56:14 +0100

    Setting up the project everything works good now

---
+++ neoforge/src/main/java/net/narutoxboruto/neoforge/MainNeoForge.java
@@ -0,0 +1,13 @@
+package net.narutoxboruto.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.narutoxboruto.Main;
+
+@Mod(Main.MOD_ID)
+public final class MainNeoForge {
+    public MainNeoForge() {
+        // Run our common setup.
+        Main.init();
+    }
+}
