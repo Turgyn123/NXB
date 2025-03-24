db064fa584c6916a5a51136b2cb7ad31cc6014ce
Pirata <19861321+Pirata156@users.noreply.github.com>
2025-03-24 02:11:08 +0000

    Refactor the testing code
    Took the testing code for the item and the tab initially made and
    refactor them to be more ordered and with reusable parts while
    testing my own setup for running the client.
    Re-updated the gitignore.
    Tested Mods MetaInfo filling

--- common/src/main/java/net/narutoxboruto/Main.java
+++ common/src/main/java/net/narutoxboruto/Main.java
@@ -3,12 +3,21 @@ package net.narutoxboruto;
 import net.narutoxboruto.items.ModItems;
 import net.narutoxboruto.items.ModTab;
 
-public final class Main {
+public final class Main
+{
+    // The unique Mod ID, used for resource locations and registry keys.
     public static final String MOD_ID = "narutoxboruto";
 
-    public static void init() {
-        // Write common init code here.
+    /**
+     * Initializes the mod. This method is called during mod loading and is responsible for registering items,
+     * creative tabs, and other common components.
+     */
+    public static void init()
+    {
+        // Register all mod items.
         ModItems.register();
+
+        // Register the custom creative tab.
         ModTab.register();
     }
 }
